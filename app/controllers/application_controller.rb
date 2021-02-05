09a2472bf772c8001a692ff6195cde79860a5b90
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-02-05 11:17:12 +0900

    rubocop適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,29 +6,26 @@ class ApplicationController < ActionController::Base
   # ログイン済ユーザーのみにアクセスを許可する
   before_action :authenticate_user!
 
-  # deviseコントローラーにストロングパラメータを追加する          
+  # deviseコントローラーにストロングパラメータを追加する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
     # サインアップ時にnameのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:postral_code,:address,:self_introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[postral_code address self_introduction])
     # アカウント編集の時にnameとprofileのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:account_update, keys: [:postral_code,:address,:self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postral_code address self_introduction])
   end
 
+  around_action :switch_locale
 
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 
-
-    around_action :switch_locale
-
-    def switch_locale(&action)
-      locale = params[:locale] || I18n.default_locale
-      I18n.with_locale(locale, &action)
-    end
-  
-    def default_url_options
-      { locale: I18n.locale }
-    end
-
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
--- db/migrate/20210130062800_rename_postral_code_column_to_users.rb
+++ db/migrate/20210130062800_rename_postral_code_column_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenamePostralCodeColumnToUsers < ActiveRecord::Migration[6.1]
   def change
     rename_column :users, :postral_code, :postal_code
