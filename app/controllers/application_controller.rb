c3ac9f1322bc0e1a0f4fd60b7404cade951e0711
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-02-08 12:58:47 +0900

    jaとen以外はjaのページにするようにした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@
 
 class ApplicationController < ActionController::Base
   around_action :switch_locale
-
+  # locale=jaとen以外はjaを返すようにした。
   def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
+    locale = case params[:locale]
+             when 'ja'
+               'ja'
+             when 'en'
+               'en'
+             else
+               I18n.default_locale
+             end
     I18n.with_locale(locale, &action)
   end
 
