df5a6aa8bba6396734c72691d0f6af4ec5f0e162
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-02-12 01:29:28 +0900

    タイムゾーン関連の設定を削除した

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,7 @@ module BooksApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     
-    config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
-    config.active_record.default_timezone = :local
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
