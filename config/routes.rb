13d08e60448450299fa2e07ee3d11fd3f958cece
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-02-11 17:32:14 +0900

    usersのrouteをresouercesに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   devise_for :users
   resources :books
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
