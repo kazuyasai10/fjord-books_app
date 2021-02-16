5a0599048960dc1f647b60807edea1b36f2ffd52
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-02-16 17:28:31 +0900

    コールバックとメソッドの間に１行入れた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   around_action :switch_locale
-  # locale=jaとen以外はjaを返すようにした。
+
   def switch_locale(&action)
     locale = %w[ja en].include?(params[:locale]) ? params[:locale] : I18n.default_locale
     I18n.with_locale(locale, &action)
