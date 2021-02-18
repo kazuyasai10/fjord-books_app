5949c895a2d05d16bf527f9e4b4ba86b0fbeb9be
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-02-18 11:42:56 +0900

    paginationのper3は少なすぎるので削除し、デフォルトの25にした。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@
 class UsersController < ApplicationController
   def index
     if current_user.admin?
-      @users = User.order(:id).page(params[:page]).per(3)
+      @users = User.order(:id).page(params[:page])
     elsif current_user.company_admin?
-      @users = User.order(:id).where(company_id: current_user.company_id).page(params[:page]).per(3)
+      @users = User.order(:id).where(company_id: current_user.company_id).page(params[:page])
     else
       redirect_to root_path unless current_user.admin?
     end
