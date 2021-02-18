b91b098728fc778377e1e0fa255771d53b5c0da5
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-02-19 02:21:47 +0900

    admin用のコントローラーを分けた

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Admin::UsersController < ApplicationController
+  def index
+    redirect_to root_path unless current_user.admin?
+    @users = User.order(:id).page(params[:page])
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,8 @@
 
 class UsersController < ApplicationController
   def index
-    if current_user.admin?
-      @users = User.order(:id).page(params[:page])
-    elsif current_user.company_admin?
-      @users = User.order(:id).where(company_id: current_user.company_id).page(params[:page])
-    else
-      redirect_to root_path unless current_user.admin?
-    end
+    redirect_to root_path unless current_user.company_admin?
+    @users = User.order(:id).where(company_id: current_user.company_id).page(params[:page])
   end
 
   def show
