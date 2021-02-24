8709425d813cea66c15c0ea2f10e602a78334bbd
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-02-24 17:34:45 +0900

    users_controllerのindexを統一した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def index
-    redirect_to root_path unless current_user.company_admin?
-    @company = Company.find(params[:company_id])
-    @users = @company.users.order(:id).page(params[:page])
-  end
-
   def show
     @user = current_user
   end
