e4c30458f359f38cc792be1781f76a22e8b74b3e
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-03-01 13:51:51 +0900

    pathの修正

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -1,7 +1,58 @@
 # frozen_string_literal: true
 
 class Admin::CompaniesController < Admin::ApplicationController
+  before_action :set_company, only: %i[show edit update destroy]
+
   def index
     @companies = Company.all
+  end
+
+  def show; end
+
+  def new
+    @company = Company.new
+  end
+
+  def edit; end
+
+  def create
+    @company = Company.new(company_params)
+ 
+    respond_to do |format|
+      if @company.save
+        format.html { redirect_to @company, notice: 'Company was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @company.update(company_params)
+        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @company.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_companies_url, notice: 'Company was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_company
+    @company = Company.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def company_params
+    params.require(:company).permit(:name)
   end
 end
