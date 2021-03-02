73fd75ab9a400abf741342f699d8a412686b6baa
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-03-03 00:54:49 +0900

    パーセント記法へ変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   root to: 'books#index'
   devise_for :users
   resources :books
-  resources :users, only: [:index,:show]
+  resources :users, only: %i[index show]
 
   namespace :admin do
-    resources :users, only: [:index,:edit]
+    resources :users, only: %i[index edit]
     resources :companies
   end
 
   resources :companies do
-    resources :users, only: [:index], module: "companies"
+    resources :users, only: %i[index], module: "companies"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
